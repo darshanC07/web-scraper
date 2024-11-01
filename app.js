2b42f7f8a90fea8c8e32aa1c104e74483b0cca9f
darshan <darshanchoudhary2007@gmail.com>
2024-11-01 13:08:37 +0530

    completed front end to display scraped data

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 async function fetchMovies() {
-    try {
-      const response = await fetch('http://localhost:3000/movieDetails');
-      const data = await response.json();
-      console.log(data)
-      const movie = document.getElementById('data');
-      movie.innerHTML = 'name = '+data.title+"\n rating = "+data.rating;
-      
-    } catch (error) {
-      console.error('Error fetching movies:', error);
-    }
+  try {
+    const response = await fetch('http://localhost:3000/movieDetails');
+    const data = await response.json();
+    console.log(data)
+    document.getElementById("title").innerText = data.title;
+    document.getElementById("releaseYear").innerText = `Released: ${data.releaseYear}`;
+    document.getElementById("director").innerText = data.director;
+    document.getElementById("desc").innerText = data.desc;
+
+    const castListElement = document.getElementById("castList");
+    data.casts.forEach(cast => {
+      const li = document.createElement("li");
+      li.innerText = cast;
+      castListElement.appendChild(li);
+    });
+
+  } catch (error) {
+    console.error('Error fetching movies:', error);
   }
-  
+}
+fetchMovies()
